f8c224ebdc41326d33fedfff2b65f942863fc267
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-06-14 18:16:28 -0400

    Bugfixing - Handling 0

--- collatz.py
+++ collatz.py
@@ -1,20 +1,22 @@
 def collatz(number):
-
-        if number != 1:
-            if number % 2 == 0: #If # is even, divide by 2
-                number = number // 2
-                print(number)
-            elif number % 2 == 1: #If # is odd, do this sequence
-                number = 3 * number + 1
-                print(number)
-        elif number == 1:
-            return # Exit the loop
+    if number == 0:
+        print("You can't enter 0!")
+        return
+    if number != 1:
+        if number % 2 == 0: #If # is even, divide by 2
+            number = number // 2
+            print(number)
+        elif number % 2 == 1: #If # is odd, do this sequence
+            number = 3 * number + 1
+            print(number)
+    elif number == 1:
+        return # Exit the loop
         
-        collatz(number) # Call the function recursively
+    collatz(number) # Call the function recursively
 
 while True:
     try:
         value = int(input("Input: "))
         collatz(value)
     except ValueError:
-        print("Invalid, enter an integer instead")
+        print("Enter a valid integer instead")
